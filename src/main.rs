0dd974c44cf7793ad48f1e00e982e971d612847a
Sam Roelants <sam@samroelants.com>
2024-10-27 11:08:47 +0100

    Refactors

--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,113 @@ use std::fmt::Display;
 use std::io::Write;
 use std::path::PathBuf;
 
-use scanner::Token;
+use scanner::{ScanError, Scanner, Token};
 use colors::{NORMAL, RED};
 
 mod scanner;
-mod colors;
+pub mod colors;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut interpreter = Loxide::new();
 
     if args.len() > 2 {
         println!("Usage: loxide [script]");
         std::process::exit(64);
     } else if args.len() == 2 {
-        run_file(&args[1]);
+        interpreter.run_file(&args[1]);
     } else {
-        run_prompt();
+        interpreter.run_prompt();
     }
 }
 
-fn run_file(file: &str) {
-    let Ok(input) = std::fs::read_to_string(PathBuf::from(file)) else {
-        eprintln!("[{RED}ERR{NORMAL}]: File not found: {file}");
-        return;
-    };
-
-    if let Err(err) = run(&input) {
-        eprintln!("{err}");
-    };
+struct Loxide {
+    found_error: bool,
 }
 
-fn run_prompt() {
+impl Loxide {
+    pub fn new() -> Self {
+        Self { found_error: false }
+    }
+
+    pub fn run_file(&mut self, file: &str) {
+        let Ok(input) = std::fs::read_to_string(PathBuf::from(file)) else {
+            eprintln!("[{RED}ERR{NORMAL}]: File not found: {file}");
+            return;
+        };
+
+        if let Err(err) = self.run(&input) {
+            eprintln!("{err}");
+            self.found_error = true;
+            std::process::exit(65);
+        };
+    }
+
+    pub fn run_prompt(&mut self) {
+        print_prompt();
+
+        for line in std::io::stdin().lines() {
+            let Ok(line) = line else {
+                eprintln!("[{RED}ERR{NORMAL}] Failed to read input");
+                print_prompt();
+                continue;
+            };
+
+
+            if let Err(err) = self.run(&line) {
+                eprintln!("{err}");
+            };
+
+            print_prompt();
+        }
+    }
+
+    pub fn run(&mut self, input: &str) -> Result<(), LoxideError> {
+        let mut scanner = Scanner::new(input);
+        scanner.scan_tokens();
+
+        for token in scanner.tokens() {
+            println!("{token:?}");
+        }
+
+        Ok(())
+    }
+}
+
+fn print_prompt() {
     print!("> ");
     std::io::stdout().flush().unwrap();
-
-    for line in std::io::stdin().lines() {
-        let Ok(line) = line else {
-            eprintln!("[{RED}ERR{NORMAL}] Failed to read input");
-            print!("> ");
-            std::io::stdout().flush().unwrap();
-            continue;
-        };
-
-
-        if let Err(err) = run(&line) {
-            eprintln!("{err}");
-        };
-    }
 }
 
-fn run(input: &str) -> Result<(), LoxideError> {
-    println!("{input}");
-
-    Ok(())
+pub struct FileNotFoundError<'a> {
+    path:  &'a str,
 }
 
-pub struct LoxideError<'a> {
+pub struct ReadFailedError;
+
+pub struct ParseError<'a> {
     token: Token<'a>,
-    msg: &'static str,
+}
+
+pub enum LoxideError<'a> {
+    FileNotFoundError(FileNotFoundError<'a>),
+    ReadFailedError(ReadFailedError),
+    ScanError(ScanError),
+    ParseError(ParseError<'a>),
 }
 
 impl<'a> Display for LoxideError<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{RED}ERR{NORMAL}] {line}:{col}: {msg}", line = self.token.line, col = self.token.col, msg = self.msg)
+        match self {
+            LoxideError::FileNotFoundError(err) =>
+                write!(f, "[{RED}ERR{NORMAL}] File not found: {path}", path = err.path),
+            LoxideError::ReadFailedError(_) =>
+                write!(f, "[{RED}ERR{NORMAL}] Failed to read input"),
+            LoxideError::ScanError(err) =>
+                write!(f, "{err}"),
+            LoxideError::ParseError(err) =>
+                write!(f, "[{RED}ERR{NORMAL}] {line}:{col} Unexpected token {tok}", line = err.token.line, col = err.token.col, tok = err.token.lexeme),
+            _ => Ok(())
+        }
     }
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,24 @@
+use crate::colors::{NORMAL, RED};
+use std::fmt::Display;
+
 #[derive(Debug)]
+pub struct ScanError {
+    line: usize,
+    col: usize,
+    ch: char,
+}
+
+impl Display for ScanError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{RED}ERR{NORMAL}] {line}:{col} Unexpected character '{ch}'", line = self.line, col = self.col, ch = self.ch)
+    }
+}
+
+impl std::error::Error for ScanError {}
+
+type ScanResult = Result<(), ScanError>;
+
+#[derive(Debug, Copy, Clone)]
 pub enum TokenType {
     // Single character tokens
     LeftParen,
@@ -26,7 +46,7 @@ pub enum TokenType {
     // Literals
     Identifier,
     String,
-    NUMBER,
+    Number,
 
     // Keywords
     And,
@@ -49,7 +69,7 @@ pub enum TokenType {
     Eof,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Token<'a> {
     pub token_type: TokenType,
     pub lexeme: &'a str,
@@ -57,6 +77,95 @@ pub struct Token<'a> {
     pub col: usize,
 }
 
+impl<'a> Token<'a> {
+    pub fn new(token_type: TokenType, lexeme: &'a str, line: usize, col: usize) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            line,
+            col,
+        }
+    }
+}
+
 pub struct Scanner<'a> {
-    input: &'a str,
+    source: &'a str,
+    tokens: Vec<Token<'a>>,
+    start: usize,
+    current: usize,
+    line: usize,
+    col: usize,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Self {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+            col: 1,
+        }
+    }
+
+    pub fn tokens(&self) -> &[Token] {
+        self.tokens.as_slice()
+    }
+
+    pub fn completed(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    pub fn scan_tokens(&mut self) {
+        while !self.completed() {
+            self.start = self.current;
+            if let Err(err) = self.scan_token() {
+                eprintln!("{err}");
+            };
+        }
+
+        self.tokens
+            .push(Token::new(TokenType::Eof, "", self.line, self.col));
+    }
+
+    fn scan_token(&mut self) -> ScanResult {
+        use TokenType::*;
+
+        let ch = self.advance();
+
+        let token_type = match ch {
+            '(' => LeftParen,
+            ')' => RightParen,
+            '{' => LeftBrace,
+            '}' => RightBrace,
+            ',' => Comma,
+            '.' => Dot,
+            '-' => Minus,
+            '+' => Plus,
+            ';' => Semicolon,
+            '*' => Star,
+
+            // TODO: Keep matching until we get a maximal set of unrecognized characters,
+            // so we can report them all in one go
+            _ => return Err(ScanError { line: self.line, col: self.col, ch }),
+        };
+
+        let substr = &self.source.as_bytes()[self.start..self.current];
+        let substr = std::str::from_utf8(substr).unwrap();
+
+        // NOTE: I have to to the pushing inside this method to keep the borrow
+        // checker happy. (It can't see the non-overlapping partial borrows if
+        // we borrow from self.source and self.tokens at the same time.)
+        let token = Token::new(token_type, substr, self.line, self.col);
+        self.tokens.push(token);
+
+        Ok(())
+    }
+
+    fn advance(&mut self) -> char {
+        let ch = self.source.as_bytes()[self.current].into();
+        self.current += 1;
+        ch
+    }
 }
